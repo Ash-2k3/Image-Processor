6415cce1627ea8338ff32f8ea31e34142544032f
Ash-2k3 <ash.me.cse@gmail.com>
2023-02-16 17:43:50 +0530

    Just testing stuff

--- main.py
+++ main.py
@@ -3,14 +3,15 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+from PIL import Image, ImageFilter
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+img = Image.open('./sample_Images/pikachu.jpg')
+filtered_img = img.filter(ImageFilter.BLUR)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(dir(img))
+    filtered_img.save("blur.png", "png")
+    filtered_img.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
